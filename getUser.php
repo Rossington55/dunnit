59f1eeb0cf73ffea4becde19b7bd646ebd7a6823
Harrison Feldman <hcfeldman@outlook.com>
2020-06-29 15:32:59 +1000

    test userid

--- getUser.php
+++ getUser.php
@@ -5,13 +5,15 @@
 	
 	if(isset ($_GET["id"])){
 		$userId = sanitise_input($_GET["id"]);
+		echo "<p>$userId</p>";
 	}else{
 		echo "<p>Error: No user id given</p>";
 	}
 
+	/*
 	$query = "SELECT 1 FROM users WHERE user_id = '$userId''";
 	$result = db($query,false);
 
 	ob_clean();
-	echo $result;
+	echo $result;*/
 ?>
